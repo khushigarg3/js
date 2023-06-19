03d1ec27ad5d18551349793f1e4c2d886fcb51a0
khushigarg3 <106480291+khushigarg3@users.noreply.github.com>
2023-06-19 04:50:42 +0000

    conversion_operation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,21 @@
+let score = "33acb"
+
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber);
+ 
+// "33" -> 33
+// "33abc" -> Nan
+//  true -> 1 , false -> 0
+ 
+let isLoggedIn  = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 -> true
+// "" -> false
+//  "xyz" -> true
