7d68e7aad2338bdd5399d0453b96abf7ca62e430
khushigarg3 <106480291+khushigarg3@users.noreply.github.com>
2023-06-19 04:37:38 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";  // treat all js code as newer version
+
+// alert("hello")
+// alert(3+9)            we are using node.js not browser
+
+console.log("hlo");  console.log(2+4)  // code readibility should be high
+
+let name = "khushi"
+let age = 34
+let isLoggedIn = true
+let state;  // undefined
+
+
+// primitive datatypes :-
+// number => 2 to power 53
+// bigint
+// string => ""  ''
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol =>unique 
+
+// object
+
+console.log(typeof "khushi")
+console.log(typeof age)
+
+console.log(typeof null)
+
+console.log(typeof undefined)
