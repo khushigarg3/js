670f2fd5cfb33e5fb25813fd4b534f0517a8a7a3
khushigarg3 <106480291+khushigarg3@users.noreply.github.com>
2023-06-26 18:53:23 +0000

    config done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+const myArr = [0,1,2,3,4,5,true,"khushi"]
+const heroes = ["shaktiman","pokemon","naagraj"]
+const arr = new Array(1,2,1,2,3,4)
+console.log(myArr[6]);
+
+// Array methods
+
+myArr.push(6)
+console.log(myArr);
+
+myArr
+
+
+
+
