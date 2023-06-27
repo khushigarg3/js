67bd9d072815de204ee4731c8893f992eee94f6a
khushigarg3 <106480291+khushigarg3@users.noreply.github.com>
2023-06-27 02:45:27 +0000

    Arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -8,8 +8,31 @@ console.log(myArr[6]);
 myArr.push(6)
 console.log(myArr);
 
-myArr
+myArr.pop()
+console.log(myArr);
 
+myArr.unshift(9);
+console.log(myArr);
 
+myArr.shift()
+console.log(myArr);
 
+console.log(myArr.includes(5));
+console.log(myArr.indexOf(5));
 
+const newArr = myArr.join()
+console.log(myArr)
+console.log(newArr)
+console.log(typeof newArr)
+
+// slice, splice
+console.log("A ",myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+console.log("C ",myArr);
