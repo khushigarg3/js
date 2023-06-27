401aa16f83ed40c56f97f93068723695c4bf0bd8
khushigarg3 <106480291+khushigarg3@users.noreply.github.com>
2023-06-27 10:50:26 +0000

    config done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -4,3 +4,56 @@ const tinderUser = new Object()
 tinderUser.id = "123abc"
 tinderUser.name = "charu"
 tinderUser.isLoggedIn = false
+console.log(tinderUser);
+
+const user = {
+    email :"khushi garg",
+    fullname:{
+        userfullname:{firstname:"khushi",
+        lastname:"garg"
+            }
+    }
+}
+
+console.log(user.fullname?.userfullname.lastname);
+
+const obj1 = {
+    1:"a",
+    2:"b"
+}
+
+const obj2 = {
+    3:"h"
+}
+
+// const obj3 = {obj1, obj2}
+// console.log(obj3)
+
+// const obj3 = Object.assign({}, obj1 ,obj2)
+// console.log(obj3)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+const user2 = [
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:1,
+        email:"h@gmail.com"
+    }
+]
+
+console.log(user2[1].email);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
