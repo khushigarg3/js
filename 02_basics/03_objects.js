87d64ed10a53c9c345248b00bffcbaf193355326
khushigarg3 <106480291+khushigarg3@users.noreply.github.com>
2023-06-27 06:20:22 +0000

    config done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,3 +1,50 @@
 // singleton
 
 // object literals
+
+const mySym = Symbol("key1")
+
+// Object.create
+const user = {
+    name:"khushi",
+    "full name":"garg",
+    [mySym]:"mykey1",
+    age: 18,
+    location:"jaipur",
+    email:"khushi@gmail.com",
+    isLoggedIn: false,
+    lastLogedInDays :["mon","wed"]
+}
+
+console.log(user.email);
+
+console.log(user["email"]);
+
+//console.log(user.full name);  not useful
+console.log(user["full name"]);
+
+console.log(user[mySym]);
+
+user.email = "garg@gmail.com"
+// Object.freeze(user)
+user.email = "gargkhushi@gmail.com"
+console.log(user)
+
+
+user.greeting = function(){
+    console.log("hlo js user");
+}
+
+user.greeting2 = function(){
+    console.log(`hello js user,${this.name}`);
+}
+
+console.log(user.greeting)
+console.log(user.greeting())
+
+console.log(user.greeting2())
+
+
+
+
+
